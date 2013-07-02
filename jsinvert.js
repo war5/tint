1ad05c4026c4682648d5665535f27dc1ca133042
clayton_Ward <war5@pdx.edu>
2013-07-01 22:30:13 -0700

    please work

--- jsinvert.js
+++ jsinvert.js
@@ -1,8 +1,13 @@
 
-function doSomething() {
+// Copywrite © Clayton Ward 2013
+// this program is under the "MIT license"
+// please see the file "license" in the source
+// distribution of this software for license terms
+function dosomething(canvas_pass,picture_pass)
+{
     var ctx=document.getElementById("testcanvas").getContext("2d");
     
-    var img=document.getElementById("myimage");
+    var img=document.getElementById("rainbow_rose");
     var x=0,y=0,width=img.width,height=img.height;
     ctx.drawImage(img,x,y,width,height);
     
@@ -13,7 +18,7 @@ function doSomething() {
 	pix[i  ] = 255 - pix[i  ]; // red
 	pix[i+1] = 255 - pix[i+1]; // green
 	pix[i+2] = 255 - pix[i+2]; // blue
-	// i+3 is alpha (the fourth element) 
+	
     }  
     
     ctx.putImageData(imgd, x, y); 
@@ -21,6 +26,12 @@ function doSomething() {
 
 function test_script()
 {
-   confirm("this works!"); 
+    confirm("this works!"); 
     console.log("this is working!");
+}
+
+function input_box()
+{
+    var text=document.getElementById("myinput").value;
+    confirm(text);
 }
